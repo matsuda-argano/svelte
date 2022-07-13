70ef329d1859376608d9f276543b313802d8f3ea
matsuda-argano <matsuda@argano.co.jp>
2022-07-14 08:27:55 +0900

    first component

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,18 @@
 <script lang="ts">
-  import TailwindCss from './TailwindCSS.svelte'
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import TailwindCss from './TailwindCSS.svelte';
+  import Header from './components/Header.svelte';
+
+  export let name: string;
 </script>
 
 <TailwindCss />
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+<Header />
+<main class="pt-16">
+  <div class="container mx-auto px-6 my-4">
+    <h1>Hello {name}!</h1>
+    <p>
+      Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte
+      apps.
+    </p>
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
