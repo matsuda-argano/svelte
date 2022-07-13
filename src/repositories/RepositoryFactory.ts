2597891675979f39c3f21dad2bbccf6f5c3aab72
matsuda-argano <matsuda@argano.co.jp>
2022-07-14 08:54:54 +0900

    book repository

---
+++ src/repositories/RepositoryFactory.ts
@@ -0,0 +1,12 @@
+import { BookRepository } from './book';
+import type { BookRepositoryInterface } from './book';
+
+export const BOOK = Symbol('book');
+
+export interface Repositories {
+  [BOOK]: BookRepositoryInterface;
+}
+
+export default {
+  [BOOK]: new BookRepository()
+} as Repositories;
